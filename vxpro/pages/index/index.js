62fa7fa6c5b09adb0777efc4d886566d704ec215
ASFBHKASJGFK <48707793+ASFBHKASJGFK@users.noreply.github.com>
2019-06-12 17:53:57 +0800

    Add files via upload

--- vxpro/pages/index/index.js
+++ vxpro/pages/index/index.js
@@ -1,54 +1,120 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+
+  /**
+   * 页面的初始数据
+   */
+  data:{
+    data:[
+      {
+        name: "陈吉凡", state: "进行", time: "2018-09-30 14:00-16:00", status: "跑腿", url: "../../icons/1.jpg", money: "132",
+        taskid: "1"
+      },
+      {
+        name: "陈金坤", state: "进行", time: "2018-10-12 18:00-20:00", status: "跑腿", url: "../../images/bad3.jpg", money: "205",
+        taskid: "2"
+      },
+      {
+        name: "陈泓霖", state: "进行", time: "2018-09-30 14:00-16:00", status: "跑腿", url: "../../icons/1.jpg", money: "132",
+        taskid: "3"
+      },
+      {
+        name: "陈吉凡傻逼", state: "进行", time: "2018-09-30 14:00-16:00", status: "跑腿", url: "../../icons/1.jpg", money: "132",
+        taskid: "4"
+      },
+      {
+        name: "弱智陈吉凡", state: "进行", time: "2018-09-30 14:00-16:00", status: "跑腿", url: "../../icons/1.jpg", money: "132",
+        taskid: "5"
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    ],
+    
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  },
+  
+  discrib: function (e) {
+    console.log(e.currentTarget.dataset.task)
+    var data = e.currentTarget.dataset.task
+    wx.setStorageSync('data', data)
+    wx.navigateTo({
+
+      url: "../logs/logs"
+
     })
+
+
+
+  },
+  jieshou: function (e) {
+    console.log(e.target.dataset.taskid)
+    wx.showModal({
+      title: '确定接收该任务？',
+      content: '接收任务后请进入-进行中-查看',
+      success: function (res) {
+        if (res.confirm) {
+          console.log(e.target.dataset.taskid)
+        }
+      }
+    })
+    /*wx.navigateTo({
+    })*/
   }
+
+
 })
+
+
+
