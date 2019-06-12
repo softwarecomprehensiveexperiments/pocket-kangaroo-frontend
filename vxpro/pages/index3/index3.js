d11f0dd36cdb5d0f7e5af9a745ef46650bbf7d87
ASFBHKASJGFK <48707793+ASFBHKASJGFK@users.noreply.github.com>
2019-06-12 17:50:01 +0800

    Add files via upload

--- vxpro/pages/index3/index3.js
+++ vxpro/pages/index3/index3.js
@@ -1,66 +1,200 @@
-// pages/index3/index3.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-    msg:"hello world 1"
+    currtab: 0,
+    swipertab: [
+      {
+        name: '接收', index: 0
+      },
+      {
+        name: '发布', index: 1
+      }
+    ],
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
-  },
-
+  onLoad: function (options) { },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    // 页面渲染完成
+    this.getDeviceInfo()
+    this.orderShow()
   },
 
+  getDeviceInfo: function () {
+    let that = this
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          deviceW: res.windowWidth,
+          deviceH: res.windowHeight
+        })
+      }
+    })
+  },
   /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  * @Explain：选项卡点击切换
+  */
+  tabSwitch: function (e) {
+    var that = this
+    if (this.data.currtab === e.target.dataset.current) {
+      return false
+    } else {
+      that.setData({
+        currtab: e.target.dataset.current
+      })
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  tabChange: function (e) {
+    this.setData({
+      currtab: e.detail.current
+    })
+    this.orderShow()
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  orderShow: function () {
+    let that = this
+    switch (this.data.currtab) {
+      case 0:
+        that.alreadyShow()
+        break
+      case 1:
+        that.waitPayShow()
+        break
+    }
+  },
+  alreadyShow: function () {
+    this.setData({
+      alreadyOrder: [
+        {
+          name: "陈吉凡", state: "进行", time: "2018-09-30 14:00-16:00", status: "跑腿", url: "../../icons/1.jpg", money: "132",
+          title:"东校区救命", short_description: "我要爆炸了，有没...", 
+        taskid:"1" ,discrib:""
+        },
+        {
+          name: "陈金坤", state: "进行", time: "2018-10-12 18:00-20:00", status: "跑腿", url: "../../images/bad3.jpg", money: "205",
+        taskid: "2"
+        },
+        {
+          name: "陈泓霖", state: "进行", time: "2018-09-30 14:00-16:00", status: "跑腿", url: "../../icons/1.jpg", money: "132",
+         taskid:"3"
+        },
+        {
+          name: "陈吉凡傻逼", state: "进行", time: "2018-09-30 14:00-16:00", status: "跑腿", url: "../../icons/1.jpg", money: "132",
+          taskid: "4"
+        },
+        {
+          name: "弱智陈吉凡", state: "进行", time: "2018-09-30 14:00-16:00", status: "跑腿", url: "../../icons/1.jpg", money: "132",
+          taskid: "5"
+        },
+      ]
+    })
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  waitPayShow: function () {
+    this.setData({
+      waitPayOrder: [
+        {
+          name: "陈吉凡", state: "进行", time: "2018-09-30 14:00-16:00", status: "跑腿", url: "../../icons/1.jpg", money: "132",
+          taskid: "1"
+        },
+        {
+          name: "陈金坤", state: "进行", time: "2018-10-12 18:00-20:00", status: "跑腿", url: "../../images/bad3.jpg", money: "205",
+          taskid: "2"
+        },
+        {
+          name: "陈泓霖", state: "进行", time: "2018-09-30 14:00-16:00", status: "跑腿", url: "../../icons/1.jpg", money: "132",
+          taskid: "3"
+        },
+        {
+          name: "陈吉凡傻逼", state: "进行", time: "2018-09-30 14:00-16:00", status: "跑腿", url: "../../icons/1.jpg", money: "132",
+          taskid: "4"
+        },
+        {
+          name: "弱智陈吉凡", state: "进行", time: "2018-09-30 14:00-16:00", status: "跑腿", url: "../../icons/1.jpg", money: "132",
+          taskid: "5"
+        },
+      ]
+    })
+  },
+  done:function(e){
+    console.log(e.target.dataset.taskid)
+    wx.showModal({
+      title: '确定完成？',
+      content: '等待发布者确认完成后即可得到报酬',
+      success: function (res) {
+        if (res.confirm) {
+          console.log(e.target.dataset.taskid)
+        }
+      }
+    }) 
+    /*wx.navigateTo({
+    })*/
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
 
+  undo: function (e) {
+    console.log(e.target.dataset.taskid)
+    wx.showModal({
+      title: '确定放弃？',
+      content: '放弃后可以通过历史任务查看',
+      success: function (res) {
+        if (res.confirm) {
+          console.log(e.target.dataset.taskid)
+        }
+      }
+    })
+    /*wx.navigateTo({
+    })*/
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  fabudone: function (e) {
+    console.log(e.target.dataset.taskid)
+    wx.showModal({
+      title: '确定完成？',
+      content: '确认完成将支付报酬',
+      success: function (res) {
+        if (res.confirm) {
+          console.log(e.target.dataset.taskid)
+        }
+      }
+    })
+    /*wx.navigateTo({
+    })*/
+  },
+
+  fabundo: function (e) {
+    console.log(e.target.dataset.taskid)
+    wx.showModal({
+      title: '确定放弃发布？',
+      content: '放弃后可通过历史任务查看',
+      success: function (res) {
+        if (res.confirm) {
+          console.log(e.target.dataset.taskid)
+        }
+      }
+    })
+    /*wx.navigateTo({
+    })*/
+  },
+
+  discrib: function(e){
+    console.log(e.currentTarget.dataset.taskid)
+    var data = e.currentTarget.dataset.taskid
+    wx.setStorageSync('data', data)
+    wx.navigateTo({
+      
+      url:"../logs/logs"
+      
+    })
+    
+
 
   }
+  
+  
 })
