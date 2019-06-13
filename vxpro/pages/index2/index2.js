6e87163fa486207e9f514cc7e8d874484ab10c6a
ASFBHKASJGFK <48707793+ASFBHKASJGFK@users.noreply.github.com>
2019-06-13 15:32:31 +0800

    Add files via upload

--- vxpro/pages/index2/index2.js
+++ vxpro/pages/index2/index2.js
@@ -1,11 +1,49 @@
-// pages/index2/index2.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    addIconPath1: '../../icons/addIcon1.png',
+    deletePath: '../../icons/delete.png',
+    deletePath1: '../../icons/cancel.png',
+    typeArray: ['单选', '多选', '问答'],
+    newIndex: 0,
+    addIconPath: "../../icons/addIcon.png",
+    currentFatherIndex: 0,
+    questionnaireArray: [
+      {
+        "type": "SCQ",
+        "content": {
+          "description": "Which fruit do you like best?",
+          "options":
+            [
+              { "id": 1, "name": "Lua", "isSelected": false },
+              { "id": 2, "name": "Java", "isSelected": true },
+              { "id": 3, "name": "C++", "isSelected": false }
+            ]
+        }
+      },
+      {
+        "type": "MCQ",
+        "content": {
+          "description": "Which fruit do you like?",
+          "options":
+            [
+              { "id": 1, "name": "OK", "isSelected": true },
+              { "id": 2, "name": "Java", "isSelected": false },
+              { "id": 3, "name": "C++", "isSelected": true }
+            ]
+        }
+      },
+      {
+        "type": "SAQ",
+        "content": {
+          "description": "What's your name?",
+          "answer": "i dont know"
+        }
+      }
+    ],
   },
 
   /**
@@ -62,5 +100,197 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+
+  bindTypeChange: function (e) {
+    this.setData({
+      newIndex: e.detail.value
+    });
+    console.log(e.detail.value);
+    var tempArray = this.data.questionnaireArray;
+    if (this.data.newIndex == 0) {
+      var temp0 = {
+        "type": "SCQ",
+        "content": {
+          "description": "Which fruit do you like best?",
+          "options":
+            [
+              { "id": 1, "name": "Lua", "isSelected": false },
+            ]
+        }
+      };
+      tempArray.push(temp0);
+    }
+    else if (this.data.newIndex == 1) {
+      var temp0 = {
+        "type": "MCQ",
+        "content": {
+          "description": "Which fruit do you like best?",
+          "options":
+            [
+              { "id": 1, "name": "Lua", "isSelected": false },
+            ]
+        }
+      };
+      tempArray.push(temp0);
+    }
+    else if (this.data.newIndex == 2) {
+      var temp0 = {
+        "type": "SAQ",
+        "content": {
+          "description": "Which fruit do you like best?",
+        }
+      };
+      tempArray.push(temp0);
+    }
+    this.setData({
+      questionnaireArray: tempArray,
+    });
+  },
+
+  deleteSCQ: function (input) {
+    var tempIndex = input.currentTarget.dataset.id;
+    console.log(tempIndex);
+    var tempArray = this.data.questionnaireArray;
+    tempArray.splice(tempIndex, 1);
+    this.setData({
+      questionnaireArray: tempArray,
+    });
+  },
+
+  deleteMCQ: function (input) {
+    var tempIndex = input.currentTarget.dataset.id;
+    console.log(tempIndex);
+    var tempArray = this.data.questionnaireArray;
+    tempArray.splice(tempIndex, 1);
+    this.setData({
+      questionnaireArray: tempArray,
+    });
+  },
+
+  deleteSAQ: function (input) {
+    var tempIndex = input.currentTarget.dataset.id;
+    console.log(tempIndex);
+    var tempArray = this.data.questionnaireArray;
+    tempArray.splice(tempIndex, 1);
+    this.setData({
+      questionnaireArray: tempArray,
+    });
+  },
+
+  getTempFatherIndex: function (input) {
+    var tempFatherIndex = input.currentTarget.dataset.id;
+    console.log('currentFatherIndex: ' + tempFatherIndex);
+    this.setData({
+      currentFatherIndex: tempFatherIndex,
+    });
+  },
+
+  deleteOneOfSCQ: function (input) {
+    var tempFatherIndex = this.data.currentFatherIndex;
+    var tempSonIndex = input.target.dataset.id;
+    //console.log(tempSonIndex);
+    var tempArray = this.data.questionnaireArray;
+    //console.log(tempArray[tempFatherIndex].content.options[tempSonIndex]);
+    tempArray[tempFatherIndex].content.options.splice(tempSonIndex, 1);
+    this.setData({
+      questionnaireArray: tempArray,
+    });
+  },
+
+  deleteOneOfMCQ: function (input) {
+    var tempFatherIndex = this.data.currentFatherIndex;
+    var tempSonIndex = input.target.dataset.id;
+    // console.log('tempFatherIndex: ' + tempFatherIndex);
+    // console.log('tempSonIndex: ' + tempSonIndex);
+    var tempArray = this.data.questionnaireArray;
+    // console.log(tempArray[tempFatherIndex].content.options[tempSonIndex]);
+    tempArray[tempFatherIndex].content.options.splice(tempSonIndex, 1);
+    this.setData({
+      questionnaireArray: tempArray,
+    });
+  },
+
+  addSCQ: function (input) {
+    var tempIndex = input.currentTarget.dataset.id;
+    var tempArray = this.data.questionnaireArray;
+    var tempSCQ = { "id": 1, "name": "Lua", "isSelected": false };
+    console.log(tempIndex);
+    tempArray[tempIndex].content.options.push(tempSCQ);
+    this.setData({
+      questionnaireArray: tempArray,
+    });
+  },
+
+  addMCQ: function (input) {
+    var tempIndex = input.currentTarget.dataset.id;
+    var tempArray = this.data.questionnaireArray;
+    var tempMCQ = { "id": 1, "name": "Lua", "isSelected": false };
+    console.log(tempIndex);
+    tempArray[tempIndex].content.options.push(tempMCQ);
+    this.setData({
+      questionnaireArray: tempArray,
+    });
+  },
+
+  showQ: function () {
+    console.log(this.data.questionnaireArray);
+  },
+
+  bindblurSCQ: function (input) {
+    var tempIndex = input.currentTarget.dataset.id;
+    var tempArray = this.data.questionnaireArray;
+    tempArray[tempIndex].content.description = input.detail.value;
+    this.setData({
+      questionnaireArray: tempArray,
+    });
+  },
+
+  bindblurOneOfSCQ: function (input) {
+    var tempFatherIndex = this.data.currentFatherIndex;
+    var tempSonIndex = input.target.dataset.id;
+    var tempArray = this.data.questionnaireArray;
+    tempArray[tempFatherIndex].content.options[tempSonIndex].name = input.detail.value;
+    this.setData({
+      questionnaireArray: tempArray,
+    });
+  },
+
+  bindblurMCQ: function (input) {
+    var tempIndex = input.currentTarget.dataset.id;
+    var tempArray = this.data.questionnaireArray;
+    tempArray[tempIndex].content.description = input.detail.value;
+    this.setData({
+      questionnaireArray: tempArray,
+    });
+  },
+
+  bindblurOneOfMCQ: function (input) {
+    var tempFatherIndex = this.data.currentFatherIndex;
+    var tempSonIndex = input.target.dataset.id;
+    var tempArray = this.data.questionnaireArray;
+    tempArray[tempFatherIndex].content.options[tempSonIndex].name = input.detail.value;
+    this.setData({
+      questionnaireArray: tempArray,
+    });
+  },
+
+  bindblurSAQ: function (input) {
+    var tempIndex = input.currentTarget.dataset.id;
+    var tempArray = this.data.questionnaireArray;
+    tempArray[tempIndex].content.description = input.detail.value;
+    this.setData({
+      questionnaireArray: tempArray,
+    });
+  },
+
+  bindblurAnswerOfSAQ: function (input) {
+    var tempIndex = input.currentTarget.dataset.id;
+    var tempArray = this.data.questionnaireArray;
+    tempArray[tempIndex].content.answer = input.detail.value;
+    // console.log(tempArray[tempIndex].content);
+    this.setData({
+      questionnaireArray: tempArray,
+    });
+  },
 })
